87ff94edcf988240199d9a3499639362ff58ee75
Kdangelo <kdangelof@gmail.com>
2018-02-16 14:22:27 -0300

    created idv

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,7 @@
-let display = document.getElementById('searchResults');
+$(document).ready(funtion() {
+  pages(1, 1);
 
-fetch('https://pokeapi.co/api/v2/pokemon/?limit=20&offset=500')
-  .then(function(response) {
-    //Turns the the JSON into a JS object
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data);
 
-    let html = `<img src="${data.results[0].url}">
-      <p>${data.results[0].name}</p>
-    `;
+  
 
-    //Put that HTML on the page
-    display.innerHTML = html;
-  });
-
-$.getJSON('https://pokeapi.co/api/v2/pokemon-species', function(r) {
-  console.log(r);
 });
